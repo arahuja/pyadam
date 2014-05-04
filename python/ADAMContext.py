041f6dd8bfc5783de9e1252f16f75bbf320c99ac
arahuja <aahuja11@gmail.com>
2014-05-03 20:16:57 -0400

    remove unused python files

--- python/ADAMContext.py
+++
@@ -1,9 +0,0 @@
-from pyspark.rdd import RDD
-from pyspark.seralizers import MUTF8Deserializer
-
-class ADAMContext():
-    def __init__(self, context):
-        self._sc = context
-
-    def loadAsJSON(self, name):
-        return RDD(self._sc._jsc.parquetFileAsJSON(name), self._sc, MUTF8Deserializer())
